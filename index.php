d5d4fe65f0e2a538d33c93a5b166fca9255f6320
Harley0211 <harleydavevalencia@gmail.com>
2024-05-04 10:14:40 +0800

    updated code

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Side Bar</title>
+    <title>Library</title>
 </head>
 
 <body>
@@ -23,7 +23,7 @@
             </div>
             <ul class="sidebar-nav">
                 <li class="sidebar-item">
-                    <a href="books.php" class="sidebar-link">
+                    <a href="index.php?page=books" class="sidebar-link">
                         <i class="lni lni-book"></i>
                         <span>Books</span>
                     </a>
@@ -35,31 +35,31 @@
                     </a>
                     <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
                         <li class="sidebar-item">
-                            <a href="authors.php" class="sidebar-link">
+                            <a href="index.php?page=authors" class="sidebar-link">
                                 <i class="lni lni-users"></i>
                                 <span>Authors</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="publishers.php" class="sidebar-link">
+                            <a href="index.php?page=publishers" class="sidebar-link">
                                 <i class="lni lni-notepad"></i>
                                 <span>Publishers</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="members.php" class="sidebar-link">
+                            <a href="index.php?page=members" class="sidebar-link">
                                 <i class="lni lni-database"></i>
                                 <span>Members</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="borrowings.php" class="sidebar-link">
+                            <a href="index.php?page=borrowings" class="sidebar-link">
                                 <i class="lni lni-shopping-basket"></i>
                                 <span>Borrowings</span>
                             </a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="reviews.php" class="sidebar-link">
+                            <a href="index.php?page=reviews" class="sidebar-link">
                                 <i class="lni lni-magnifier"></i>
                                 <span>Reviews</span>
                             </a>
@@ -68,11 +68,35 @@
                 </li>
             </ul>
         </aside>
-        <div class="main p-3">
-            <div class="text-center">
-                <?php include 'books.php'; ?>
+        <div class="content">
+            <?php
+            if (isset($_GET['page'])) {
+                $page = $_GET['page'];
 
-            </div>
+                switch ($page) {
+                    case 'books':
+                        include 'books.php';
+                        break;
+                    case 'authors':
+                        include 'authors.php';
+                        break;
+                    case 'publishers':
+                        include 'publishers.php';
+                        break;
+                    case 'members':
+                        include 'members.php';
+                        break;
+                    case 'borrowings':
+                        include 'borrowings.php';
+                        break;
+                    case 'reviews':
+                        include 'reviews.php';
+                        break;
+                    default:
+                        echo "Page not found";
+                }
+            }
+            ?>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
